65fa6c2e1e95157d17ef77e8ee8ff6c4b92e90a9
oid85 <oid85@mail.ru>
2025-12-28 21:50:17 +0700

    init

---
+++ config.py
@@ -0,0 +1,7 @@
+deploy_path = 'C:\\Deploy'
+
+medicaments_source_path = 'C:\\Repository\\Oid85.Medicaments\\Oid85.Medicaments'
+medicaments_deploy_directory = 'Oid85.Medicaments'
+medicaments_exe_file_name = "Oid85.Medicaments.WebHost.exe"
+medicaments_service_name = 'Oid85.Medicaments'
+medicaments_appsettings_file = 'appsettings.json'
---
+++ main.py
@@ -0,0 +1,7 @@
+import oid85_medicaments
+
+if __name__ == '__main__':
+    steps = [1]
+
+    if 1 in steps:
+        oid85_medicaments.deploy()
